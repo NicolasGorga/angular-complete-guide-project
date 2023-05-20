6b0ce6511f1d6ac0cf7765458c77e08672e287bb
mi usuario <nicogorga11@gmail.com>
2023-05-20 15:56:52 +1200

    Final content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name: 'some name', content: 'some content'}];
+  newServerName = '';
+  newServerContent = '';
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './feature/shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './feature/shopping/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './feature/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './feature/recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './feature/recipe-book/recipe-detail/recipe-detail.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeBookComponent } from './feature/recipe-book/recipe-book.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    HeaderComponent,
-    RecipeBookComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
